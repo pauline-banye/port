ed5648b6e3b0b680e43d2b2daa0633d367e4e2e9
Pauline.B <paulinebanye@gmail.com>
2022-11-16 21:19:00 +0100

    arrows

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 let menuIcon = document.getElementById("menu-icon");
-
-let zuriArt = document.getElementById("zuri-art");
-let afroArt = document.getElementById("afro-art");
-let hngArt = document.getElementById("hng-art");
-let contactForm = document.getElementById("contact-form");
-let zurichatArt = document.getElementById("zurichat-art");
-let blockgamesArt = document.getElementById("blockgames-art");
-let fullstackArt = document.getElementById("fullstack-art");
-
-let zuriHead = document.getElementById("zuri-header");
-let afroHead = document.getElementById("afro-header");
-let hngHead = document.getElementById("hng-header");
 let contactHead = document.getElementById("contact-header");
-let zurichatHead = document.getElementById("zurichat-header");
-let blockgamesHead = document.getElementById("blockgames-header");
-let fullstackHead = document.getElementById("fullstack-header");
+let subTitles = document.getElementsByClassName("section_subtitle--exp");
+let subArticles = document.getElementsByClassName("art");
+let hideMe = document.getElementsByClassName("hide");
 
 let toggleNav = document.querySelector(".nav-bar");
+let headers = [...subTitles, contactHead];
 
-
-let headers = [zuriHead, afroHead, hngHead, zurichatHead, blockgamesHead, fullstackHead, contactHead];
-let subArticles = [zuriArt, afroArt, hngArt, zurichatArt, blockgamesArt, fullstackArt, contactForm];
 
 menuIcon.addEventListener("click", () => {
     menuIcon.classList.toggle('menu-open');
@@ -30,5 +16,14 @@ menuIcon.addEventListener("click", () => {
 for (let i = 0; i < headers.length; i++) {
     headers[i].addEventListener("click", () => {
         subArticles[i].classList.toggle('hidden');
+    })
+}
+
+for (let i = 0; i < subTitles.length; i++) {
+    subTitles[i].addEventListener("click", () => {
+        hideMe[i].innerHTML === "&#9650"
+        ? hideMe[i].innerHTML = "&#9660"
+        : hideMe[i].innerHTML = "&#9650";
+        console.log(hideMe);
     })
 }
